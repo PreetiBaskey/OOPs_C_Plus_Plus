5b9a615c6bb7b72580ff1af05e8ce491307de988
PreetiBaskey <38905783+PreetiBaskey@users.noreply.github.com>
2026-02-23 15:42:20 +0530

    constructor demo example

---
+++ Constructor_Demo.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Complex {
+    private:
+        int a, b;
+    public:
+        Complex() {
+            cout<<"Constructor called!!"<<endl;
+        }
+};
+
+int main() {
+    
+    Complex c1, c2, c3;
+    
+    return 0;
+}
