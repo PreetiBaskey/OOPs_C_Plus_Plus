a88216d9ac34ba2b245f28bc8df38ec47fbe6bbf
PreetiBaskey <38905783+PreetiBaskey@users.noreply.github.com>
2026-02-23 11:31:41 +0530

    class demo example

---
+++ ClassDemo.cpp
@@ -0,0 +1,30 @@
+//The only difference between structure and class is that:
+//the members of structure are by default public and
+//the members of class are by default private
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Complex {
+    private:
+        int a, b;
+    public:
+        void setData(int x, int y) {
+            a = x;
+            b = y;
+        }
+        void showData() {
+            cout<<"a : "<<a<<endl;
+            cout<<"b : "<<b<<endl;
+        }
+};
+
+int main() {
+    
+    Complex c1; //c1 is call here object
+    c1.setData(10, 20);
+    c1.showData();
+    
+    return 0;
+}
