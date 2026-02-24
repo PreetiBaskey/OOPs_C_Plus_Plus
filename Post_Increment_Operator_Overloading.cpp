4f40473b8962f28b557000282079c2b633cf8e38
PreetiBaskey <38905783+PreetiBaskey@users.noreply.github.com>
2026-02-24 09:17:07 +0530

    post increment operator overloading

---
+++ Post_Increment_Operator_Overloading.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Integer{
+    private:
+        int x;
+    public:
+        void setData(int a) {
+            x = a;
+        }
+        void showData() {
+            cout<<"x : "<<x<<endl;
+        }
+        Integer operator++() {
+            Integer i;
+            i.x = ++x;
+            
+            return i;
+        }
+        Integer operator++(int) { //post increment
+            Integer i;
+            i.x = x++;
+            
+            return i;
+        }
+};
+
+int main() {
+    Integer i1, i2;
+    i1.setData(3);
+    i1.showData();
+    
+    i2 = i1++;
+    i1.showData();
+    i2.showData();
+}
